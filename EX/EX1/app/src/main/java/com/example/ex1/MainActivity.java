cd3b4ef796cb497bd412de0c6d9028dec5b9d8e8
PhucNH <PhucNH0303@gmail.com>
2023-03-20 23:28:09 +0700

    create spiner adapter

--- EX/EX1/app/src/main/java/com/example/ex1/MainActivity.java
+++ EX/EX1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import com.example.ex1.model.SpinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Spinner sp;
+    private int[] imgs = {
+            R.drawable.a1,
+            R.drawable.a2,
+            R.drawable.a3,
+            R.drawable.a4,
+            R.drawable.a5,
+            R.drawable.a6
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        sp = findViewById(R.id.img);
+        SpinnerAdapter adapter = new SpinnerAdapter(this);
+        sp.setAdapter(adapter);
     }
 }
---
+++ EX/EX1/app/src/main/java/com/example/ex1/model/SpinnerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.ex1.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.example.ex1.R;
+
+public class SpinnerAdapter extends BaseAdapter {
+    private int[] imgs = {
+            R.drawable.a1,
+            R.drawable.a2,
+            R.drawable.a3,
+            R.drawable.a4,
+            R.drawable.a5,
+            R.drawable.a6
+    };
+    private Context context;
+
+    public SpinnerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imgs.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        View item = LayoutInflater.from(context).inflate(R.layout.item_image, viewGroup, false);
+        ImageView img = item.findViewById(R.id.img);
+
+        return item;
+    }
+}
